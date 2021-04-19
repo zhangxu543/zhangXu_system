a0cd176a6a457dd1a249263121bb5bbf4f5453ba
张旭 <15075131893@zrgj.com>
2021-04-19 22:31:21 +0800

    登录界面

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -1,32 +1,42 @@
 var app_path = "/mm";
 var app_url = "http://localhost:8080/mm";
 var base = {
-	"sex": [{
-			"id": 0,
-			"value": "男"
+    "sex": [{
+        "id": 0,
+        "value": "男"
+    },
+        {
+            "id": 1,
+            "value": "女"
+        }
+    ],
+    "AwarePunishStatus": [
+    	{
+        "id": 0,
+        "value": "奖励"
+        },
+        {
+            "id": 1,
+            "value": "处罚"
+        }
+    ],
+    "AwarePunishLevels": [
+    	{
+        "id": 0,
+        "value": "警告"
+        },
+        {
+            "id": 1,
+            "value": "留校察看"
+        },
+		{
+			"id": 2,
+			"value": "回家反省"
 		},
 		{
-			"id": 1,
-			"value": "女"
+			"id": 2,
+			"value": "休学"
 		}
-	],
-	"AwarePunishStatus": [{
-		"id": 0,
-		"value": "奖励"
-	},
-		{
-			"id": 1,
-			"value": "处罚"
-		}
-	],
-	"AwarePunishLevels": [{
-		"id": 0,
-		"value": "警告"
-	},
-		{
-			"id": 1,
-			"value": "留校察看"
-		}
-	]
+    ]
 
 }
